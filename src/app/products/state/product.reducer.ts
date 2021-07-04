ea3141134253355629d530093fdc3808fc991a6a
Nvhamilton <60369628+Nvhamilton@users.noreply.github.com>
2021-07-04 00:25:52 -0500

    added stateful product code display

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,14 +1,13 @@
 /* NgRx */
-import { createReducer, on, createAction } from '@ngrx/store';
+import { createReducer, on, createAction } from '@ngrx/store';//imports
 
-export const productReducer = createReducer(
-  { showProductCode: true },
-  on(createAction('[Product] Toggle Product Code'), state => {
+export const productReducer = createReducer(//productReducer does not change so it is a constant
+  { showProductCode: true },//initial state to show product code set to true
+  on(createAction('[Product] Toggle Product Code'), state => {//we dispatch this action later.  State gets mapped into a return obj
     return {
-      ...state,
+      ...state,//copy of state, gets mapped into the opposite of the current property with the ! marker
       showProductCode: !state.showProductCode
       // showProductCode: state.showProductCode = false,
     };
   })
-);
-
+);//interacts with a 'reduced' slice of state. It takes state as an mapped object and returns new state
